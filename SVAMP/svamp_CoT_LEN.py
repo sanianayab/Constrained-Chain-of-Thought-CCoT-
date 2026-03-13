c3b1438c9d63d4f5651b45274ee73fc8fdee0d02
Sania Nayab <70174375+sanianayab@users.noreply.github.com>
2026-03-13 14:20:57 +0100

    Update svamp_CoT_LEN.py

--- SVAMP/svamp_CoT_LEN.py
+++ SVAMP/svamp_CoT_LEN.py
@@ -10,9 +10,7 @@ from datasets import load_dataset
 def generate_answers(dataset, output_len):
     url = "http://127.0.0.1:18081/generate"
     headers = {'Content-Type': 'application/json'}
-    parameters = {"max_tokens": 512,
-            	  "temperature": 0.5
-                 }
+    parameters = {"max_new_tokens": 200}
 
 
     all_chars = [chr(i) for i in range(sys.maxunicode)]
@@ -84,7 +82,7 @@ def generate_answers(dataset, output_len):
 
     return fields, rows
 
-# Load gsm8k[test] dataset
+# Load dataset
 main_dataset = load_dataset('ChilleD/SVAMP')
 test_dataset = main_dataset['test']
 
