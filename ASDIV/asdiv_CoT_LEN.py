d6b09f9ba080b7dea24d4f8d8ad6692de8c57eb8
Sania Nayab <70174375+sanianayab@users.noreply.github.com>
2026-03-13 14:18:03 +0100

    Update asdiv_CoT_LEN.py

--- ASDIV/asdiv_CoT_LEN.py
+++ ASDIV/asdiv_CoT_LEN.py
@@ -10,8 +10,7 @@ from datasets import load_dataset
 def generate_answers(dataset, output_len):
     url = "http://127.0.0.1:18081/generate"
     headers = {'Content-Type': 'application/json'}
-    parameters = {"max_tokens": 512,
-            	  "temperature": 0.5
+    parameters = {"max_tokens": 200,
                  }
 
 
@@ -84,7 +83,7 @@ def generate_answers(dataset, output_len):
 
     return fields, rows
 
-# Load gsm8k[test] dataset
+# Load dataset
 main_dataset = load_dataset('yimingzhang/asdiv')
 test_dataset = main_dataset['test']
 
